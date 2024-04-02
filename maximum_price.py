3ac7f63373795f90de871ba09c4bffa5e9f265ef
Lowa Ewesit <lowaewesi@gmail.com>
2024-04-02 10:41:36 +0300

    write a sudo code after the dictionary list

--- maximum_price.py
+++ maximum_price.py
@@ -8,13 +8,17 @@ def find_highest_priced_product(products):
             highest_priced_product = product
     return highest_priced_product
 
-# list of products in a dictionary
+# list dictionaries called products
 products = [
     {'name': 'Rice', 'price': 100},
     {'name': 'Sugar', 'price': 200},
     {'name': 'Cooking oil', 'price': 180},
     {'name': 'Unga', 'price': 200}
 ]
+# Call find_highest_priced_product function with the products list as an argument
+#find_highest_priced_product function is expected to return the dictionary representing
+# the product with the highest price. 
+#The result is assigned to the highest_priced_product variable.
 highest_priced_product = find_highest_priced_product(products)
 print(highest_priced_product)
 
