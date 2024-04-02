97c0327dd625a1ab472f2727bc04ee9eb44a1b09
Lowa Ewesit <lowaewesi@gmail.com>
2024-04-02 10:28:37 +0300

    Comment code

--- maximum_price.py
+++ maximum_price.py
@@ -1,13 +1,13 @@
 def find_highest_priced_product(products):
-    highest_priced_product = None
-    highest_price = float('-inf')
+    highest_priced_product = None #checks if highest priced product is none.
+    highest_price = None  #checks if highest price is none
     for product in products:
-        if product['price'] > highest_price:
+        if product['price'] > highest_price:  #checks if the value of the current product is higher than the 'highest_price'
             highest_price = product['price']
             highest_priced_product = product
     return highest_priced_product
 
-# Example usage
+# list of products
 products = [
     {'name': 'Rice', 'price': 100},
     {'name': 'Sugar', 'price': 200},
