4a2380411a375c028fe96e806d41dc85be2dbafe
Lowa Ewesit <lowaewesi@gmail.com>
2024-04-02 10:13:46 +0300

    first commit

---
+++ maximum_price.py
@@ -0,0 +1,18 @@
+def find_highest_priced_product(products):
+    highest_priced_product = None
+    highest_price = float('-inf')
+    for product in products:
+        if product['price'] > highest_price:
+            highest_price = product['price']
+            highest_priced_product = product
+    return highest_priced_product
+
+# Example usage
+products = [
+    {'name': 'Rice', 'price': 100},
+    {'name': 'Sugar', 'price': 200},
+    {'name': 'Cooking oil', 'price': 150},
+    {'name': 'Unga', 'price': 200}
+]
+highest_priced_product = find_highest_priced_product(products)
+print(highest_priced_product)
